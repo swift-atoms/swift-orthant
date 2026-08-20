1c64a14908e4722ef2ceac193b04d986036241bc
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:08:04 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,14 +1,14 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 import PackageDescription
 
 let package = Package(
     name: "swift-orthant-primitives",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27"),
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         // MARK: - Namespace
@@ -48,12 +48,30 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-direction-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-equation-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-hash-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-comparison-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-finite-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ordinal-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-direction-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-equation-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-hash-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-comparison-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-finite-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ordinal-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         // MARK: - Namespace
@@ -63,7 +81,7 @@ let package = Package(
         .target(
             name: "Orthant Primitive",
             dependencies: [
-                .product(name: "Direction Primitive", package: "swift-direction-primitives"),
+                .product(name: "Direction Primitive", package: "swift-direction-primitives")
             ]
         ),
 
@@ -118,7 +136,10 @@ let package = Package(
             name: "Orthant Primitives Test Support",
             dependencies: [
                 "Orthant Primitives",
-                .product(name: "Ordinal Primitives Test Support", package: "swift-ordinal-primitives"),
+                .product(
+                    name: "Ordinal Primitives Test Support",
+                    package: "swift-ordinal-primitives"
+                ),
             ],
             path: "Tests/Support"
         ),
